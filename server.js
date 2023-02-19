d6ff7313b39df74375c58c035cd012c4360f896f
Matthias <matthias.cheng50@gmail.com>
2023-02-19 13:48:21 -0800

    refactor code + fix project structure

--- server.js
+++ server.js
@@ -1,119 +1,32 @@
-require('dotenv').config({ path: './.env' });
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const expressLayouts = require('express-ejs-layouts');
+const fileUpload = require('express-fileupload');
 const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const Recipe = require('./models/recipe');
-const User = require('./models/user');
-
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+require('dotenv').config();
+
+app.use(express.urlencoded( { extended: true } ));
 app.use(express.static('public'));
+app.use(expressLayouts);
 
-app.use(  
-  session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: false,
-  }),
-);
+app.use(cookieParser('CookingBlogSecure'));
+app.use(session({
+  secret: 'CookingBlogSecretSession',
+  saveUninitialized: true,
+  resave: true
+}));
+app.use(flash());
+app.use(fileUpload());
 
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+const routes = require('./server/routes/recipeRoutes.js')
+app.use('/', routes);
 
-mongoose.connect(process.env.DB_CONNECT, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-mongoose
-  .connect(process.env.DB_CONNECT)
-  .then(() => console.log('DB connected'))
-  .catch((err) => console.log(err));
-
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});
-
-app.use(express.static(`${__dirname}/public`));
-
-app.get('/login', (req, res) => {
-  res.render('login.ejs');
-});
-
-app.post('/login', (req, res) => {
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password,
-  });
-
-  req.login(user, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      passport.authenticate('local')(req, res, () => {
-        res.redirect('/recipes');
-      });
-    }
-  });
-});
-
-app.get('/register', (req, res) => {
-  res.render('register.ejs');
-});
-
-app.post('/register', (req, res) => {});
-
-app.get('/new', (req, res) => {
-  res.sendFile(`${__dirname}/recipe.html`);
-});
-
-app.post('/new', (req, res) => {
-  const newRecipe = new Recipe({
-    title: req.body.title,
-    description: req.body.description,
-    calories: req.body.calories,
-    carbs: req.body.carbs,
-    protein: req.body.protein,
-    ingredients: req.body.ingredients,
-    steps: req.body.steps,
-    video: req.body.video,
-    slug: req.body.title.toLowerCase().split(' ').join('-'),
-  });
-  newRecipe.save();
-  res.redirect('/recipes');
-});
-
-app.get('/recipes', (req, res) => {
-  Recipe.find({}, (err, recipes) => {
-    res.render('recipes', {
-      recipeList: recipes,
-    });
-  });
-});
-
-app.get('/recipes/:id', (req, res) => {
-  const id = req.params.id;
-  Recipe.findById(id, (err, recipe) => {
-    if (err) {
-      console.log("Error!", err);
-    } else if (recipe) {
-      res.render('recipe', {document: recipe});
-    } else {
-      console.log("No such recipe!");
-    }
-  });
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+app.listen(port, ()=> console.log(`Listening to port ${port}`));
