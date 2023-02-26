e50c3a7c8517ac6dac48fe7004aa9de4a52ecf33
Matthias <matthias.cheng50@gmail.com>
2023-02-25 22:04:38 -0800

    edit recipe WIP

--- server/models/database.js
+++ server/models/database.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopology: true});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(){
-    console.log("Connected to MongoDB")
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DB_CONNECT, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-require('./Category')
-require('./Recipe')
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to MongoDB");
+});
+
+require("./Category");
+require("./Recipe");
